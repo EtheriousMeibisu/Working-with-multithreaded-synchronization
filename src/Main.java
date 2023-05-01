427690b403c36e4a3cdc407223003a73d8c32a27
Neto test <michaelGulidow@yandex.ru>
2023-05-01 22:02:39 +0300

    first commt

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-                for (int i = 0; i < 10; i++) {
-                    System.out.println(generateRoute("RLRFR", 100));
+        for (int i = 0; i < 10; i++) {
+            System.out.println(generateRoute("RLRFR", 100));
+        }
+        new Thread(() -> {  //Поток, который выводит результат на экран
+            SortedMap<Integer, Integer> sortedMap = new TreeMap<>((o1, o2) -> o2 - o1);
+            if (sizeToFreq.size() == 10) {
+                sortedMap.putAll(sizeToFreq);
+
+                Object[] key = sortedMap.keySet().toArray();
+
+                System.out.println("Самое частое количество повторений " + key[0] + " Встретилось " + sortedMap.get(key[0]) + " раз \n" + "Другие размеры:");
+
+                Set<Map.Entry<Integer, Integer>> entries = sizeToFreq.entrySet();
+
+                for (Map.Entry<Integer, Integer> pair : entries) {
+                    Integer keys = pair.getKey();
+                    Integer value = pair.getValue();
+                    System.out.println("- " + keys + " (" + value + ")");
                 }
-
-
-           new Thread(() -> {  //Поток, который выводит результат на экран
-        SortedMap<Integer, Integer> sortedMap = new TreeMap<>((o1, o2) -> o2 - o1);
-        if (sizeToFreq.size() == 10) {
-            sortedMap.putAll(sizeToFreq);
-
-            Object[] key = sortedMap.keySet().toArray();
-
-            System.out.println("Самое частое количество повторений " + key[0] + " Встретилось " + sortedMap.get(key[0]) + " раз \n" + "Другие размеры:");
-
-            Set<Map.Entry<Integer, Integer>> entries = sizeToFreq.entrySet();
-
-            for (Map.Entry<Integer, Integer> pair : entries) {
-                Integer keys = pair.getKey();
-                Integer value = pair.getValue();
-                System.out.println("- " + keys + " (" + value + ")");
             }
-        }
-              }).start();
-        }
+        }).start();
+    }
 
 
     public static String generateRoute(String letters, int length) {
@@ -44,14 +42,11 @@ public class Main {
             if (letter == 'R') {
                 count++;
             }
-
         }
-
         letterСounter(count);
 
         }).start();
         return route.toString();
-
     }
 
     public static void letterСounter(int count) {
@@ -61,6 +56,5 @@ public class Main {
                 sizeToFreq.put(count, value + 1);
             }
         }).start();
-
     }
 }
