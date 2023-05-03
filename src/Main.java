4a5d3fdac31536a8a0a7cb75d97fa564419c0eb7
Neto test <michaelGulidow@yandex.ru>
2023-05-03 20:32:10 +0300

    second commt

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,46 @@
 import java.util.*;
 public class Main {
+    final static String LETTERS = "RLRFR";
+    final static int ROUTE_LENGTH = 100;
+    final static int AMOUNT_OF_THREADS = 1000;
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
-
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(generateRoute("RLRFR", 100));
-        }
-        new Thread(() -> {  //Поток, который выводит результат на экран
-            SortedMap<Integer, Integer> sortedMap = new TreeMap<>((o1, o2) -> o2 - o1);
-            if (sizeToFreq.size() == 10) {
-                sortedMap.putAll(sizeToFreq);
+        for (int i = 0; i < AMOUNT_OF_THREADS; i++) {
+            new Thread(() -> {
+                String rout = generateRoute(LETTERS, ROUTE_LENGTH);
+                int frequency = (int) rout.chars().filter(ch -> ch == 'R').count();
 
-                Object[] key = sortedMap.keySet().toArray();
-
-                System.out.println("Самое частое количество повторений " + key[0] + " Встретилось " + sortedMap.get(key[0]) + " раз \n" + "Другие размеры:");
-
-                Set<Map.Entry<Integer, Integer>> entries = sizeToFreq.entrySet();
-
-                for (Map.Entry<Integer, Integer> pair : entries) {
-                    Integer keys = pair.getKey();
-                    Integer value = pair.getValue();
-                    System.out.println("- " + keys + " (" + value + ")");
+                synchronized (sizeToFreq){
+                    if(sizeToFreq.containsKey(frequency)){
+                        sizeToFreq.put(frequency, sizeToFreq.get(frequency) + 1);
+                    }else {
+                        sizeToFreq.put(frequency,1);
+                    }
                 }
-            }
-        }).start();
+            }).start();
+        }
+            Map.Entry<Integer, Integer> maxCount = sizeToFreq
+                    .entrySet()
+                    .stream()
+                    .max(Map.Entry.comparingByValue())
+                    .get();
+        System.out.println("Самое частое количество повторений " + maxCount.getKey()
+        + " Встретилось ("+  maxCount.getValue() + " раз )");
+
+        System.out.println("Другие размеры :");
+        sizeToFreq.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEach(к -> System.out.println(" - " + к.getKey() + " (" + к.getValue() + " раз)"));
     }
-
-
     public static String generateRoute(String letters, int length) {
 
-
-        Random random = new Random();
-        StringBuilder route = new StringBuilder();
-
-        new Thread(() -> { //Поток генерирующий текст
-        int count = 0;
-        for (int i = 0; i < length; i++) {
-            char letter = letters.charAt(random.nextInt(letters.length()));
-            route.append(letter);
-            if (letter == 'R') {
-                count++;
+            Random random = new Random();
+            StringBuilder route = new StringBuilder();
+            for (int i = 0; i < length; i++) {
+                route.append(letters.charAt(random.nextInt(letters.length())));
             }
-        }
-        letterСounter(count);
-
-        }).start();
-        return route.toString();
-    }
-
-    public static void letterСounter(int count) {
-        new Thread(() -> {  // Поток, который считает количество букв
-            synchronized (sizeToFreq) {
-                int value = sizeToFreq.containsKey(count) ? sizeToFreq.get(count) : 0;
-                sizeToFreq.put(count, value + 1);
-            }
-        }).start();
+            return route.toString();
     }
 }
